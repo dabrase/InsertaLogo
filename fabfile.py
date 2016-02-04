385f89a0453b3984b040b70b1f86d96e98e4aa76
Miguel Angel GV <magvugr@gmail.com>
2016-02-04 23:23:45 +0100

    revisando

--- fabfile.py
+++ fabfile.py
@@ -1,44 +1,33 @@
 from fabric.api import run, local, hosts, cd
 
-#infomacion del host
-def host_info():
-    run('uname -s')
-
-#descarga de la aplicacion utilizando git
-def get_app():
-	run('sudo apt-get update')
-	run('sudo apt-get install -y git')
-	run('sudo git clone https://github.com/magvugr/InsertaLogo.git')
-
-#Instalacion necesaria para host virgen
-def install():
-	run('cd InsertaLogo && make install')
-
-def remove():
-    run('sudo rm -r InsertaLogo')
-
-
-#Ejecucion de test
-def test_app():
-	run('cd InsertaLogo && make test')
-
-#Ejecucion de la aplicacion
-def run_app():
-	run('cd InsertaLogo/InsertaLogo && python __main__.py')
-
-#Peticion
-def peticion():
-	run('curl http://localhost:80/')
-
-
-
-#Ejecucion remota del docker
-#Instalacion de docker y descarga de imagen
-def get_docker():
+########         Docker y descargar imagen Inserta Logo
+def getdocker():
 	run('sudo apt-get update')
 	run('sudo apt-get install -y docker.io')
 	run('sudo docker pull magvugr/insertalogo')
 
-#Ejecucion de docker
-def run_docker():
+########         Ejecutamos Docker
+def rundocker():
 	run('sudo docker run -i -t magvugr/insertalogo')
+
+########         Clonamos repositorio
+def getapp():
+	run('sudo apt-get update')
+	run('sudo apt-get install -y git')
+	run('sudo git clone https://github.com/magvugr/InsertaLogo.git')
+
+########         Install del make
+def install():
+	run('cd InsertaLogo && make install')
+
+########         Test
+def test():
+	run('cd InsertaLogo && make test')
+
+########         Ejecucion app
+def run_app():
+	run('cd InsertaLogo/InsertaLogo && python manage.py runserver 8000')
+
+########         Borrar
+def remove():
+    run('sudo rm -r InsertaLogo')
