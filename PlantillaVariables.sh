e643648fd1467b5e5f88fc23b7271c34386d0779
Miguel Angel GV <magvugr@gmail.com>
2016-02-08 18:29:31 +0100

    Plantilla Variables de Entorno

---
+++ PlantillaVariables.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+#VariablesEntorno
+
+export USERN="" #Nombre de Usuario
+export PASSW="" #Pass de Usuario
+export MAQSER="" #Nombre de Maquina y servicio
+export CERTIFICADOM="" #Certificado
+export SUBCRIP="" #Subscripcion Azure
+export USERNSSH="" #Usuario Ssh
+export PASSWSSH="" #Pass ususario ssh
